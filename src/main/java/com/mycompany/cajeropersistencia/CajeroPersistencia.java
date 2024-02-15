90f19014d6fadd7eb33121cc0c33c571765ad0d6
Puert4 <jullian.puerta248239@potros.itson.edu.mx>
2024-02-14 21:33:00 -0800

    Creando CajeroPeristnecias

---
+++ src/main/java/com/mycompany/cajeropersistencia/CajeroPersistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cajeropersistencia;
+
+/**
+ *
+ * @author TeLesheo
+ */
+public class CajeroPersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
